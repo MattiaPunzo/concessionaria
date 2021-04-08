a43cfe9d6a14fc7dafc5ed91007f3d3a0343d1bd
MattiaPunzo <mattiapunzo@hotmail.it>
2021-04-08 15:33:10 +0200

    push auto.html

--- src/main/java/it/begear/heroku/concessionaria/repository/AutoRepository.java
+++ src/main/java/it/begear/heroku/concessionaria/repository/AutoRepository.java
@@ -11,6 +11,5 @@ public interface AutoRepository extends JpaRepository<Auto, Integer> {
 	@Query("SELECT a FROM Auto a WHERE CONCAT(a.marca, a.modello , a.colore, a.prezzo) LIKE %?1%")
 	public List<Auto> search(String keyword);
 	
-	@Query("SELECT a FROM Auto a ORDER BY a.marca")
-	public List<Auto> orderByMarca();
+	
 }
